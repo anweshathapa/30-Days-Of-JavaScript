0cca4a428e962392bd0ca128215a687bc8c149b8
Anwesha Thapa <anweshathapa15@gmail.com>
2025-09-06 02:50:43 +0530

    Day 7

--- Day 7.js
+++ Day 7.js
@@ -1,7 +1,7 @@
 //LEETCODE 2626 Array Reduce Transformation
 // QUESTION. Given an integer array nums, a reducer function fn, and an initial value init, 
-return the final result obtained by executing the fn function on each element of the array,
-sequentially, passing in the return value from the calculation on the preceding element.//
+// return the final result obtained by executing the fn function on each element of the array,
+// sequentially, passing in the return value from the calculation on the preceding element.
 
 //*************************************SOLUTION*********************************************//
 
