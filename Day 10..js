fe652a4259053a5b731d9241066db0515303fcb3
Anwesha Thapa <anweshathapa15@gmail.com>
2025-09-06 02:41:01 +0530

    Day 10

---
+++ Day 10..js
@@ -0,0 +1,17 @@
+//**** LEETCODE 2666. Allow One Function Call
+// Question: Given a function fn, return a new function that is identical to the original function except that it ensures fn is called at most once.
+
+// ******************************************** Solution ****************************************************************
+
+function once(fn){
+    let called= false;
+    let result;
+    return function(...args){
+        if(!called){
+          result= fn(...args);
+          called= true;
+          return result;
+        }
+        return undefined;
+    }
+}
